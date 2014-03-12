2ddd3d301dfff3062059ad5ec5afb3631eba8852
Dom Chapman <viralplatipuss@gmail.com>
2014-03-12 13:09:33 +0000

    Setup Xcode project.

---
+++ Thoughts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Thoughts
+//
+//  Created by Dom Chapman on 3/12/14.
+//  Copyright (c) 2014 Dom Chapman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "THAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([THAppDelegate class]));
+    }
+}
